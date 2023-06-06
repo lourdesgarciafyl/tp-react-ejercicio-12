de0a32d9c80d6ad1a3439073b728ad8178636156
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-06-06 15:11:54 -0300

    se escribe nuevamente la peticion a la API

--- src/components/FormularioNoticias.jsx
+++ src/components/FormularioNoticias.jsx
@@ -1,45 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Form, Button} from "react-bootstrap";
 import BloqueNoticias from "./BloqueNoticias";
-import { useForm } from "react-hook-form";
 
 const FormularioNoticias = () => {
   const [pais, setPais] = useState("")
   const [categoria, setCategoria] = useState("")
   const [noticias, setNoticias] = useState([]);
-  const {register, formState: {errors}, reset , handleSubmit} = useForm()
+  let API = `https://newsdata.io/api/1/news?apikey=pub_24021f4291fa33aa976aa3dddd22e4c75cb40&language=es`
 
-  const consultarApi = async (datos = "") =>{
-    try{
-      if(datos === ""){
-        const respuesta = await fetch("https://newsdata.io/api/1/news?apikey=pub_24021f4291fa33aa976aa3dddd22e4c75cb40")
-        const data = await respuesta.json()
-        console.log(data.results)
-        setNoticias(data.results)
-      }else{
-        const respuesta = await fetch(`https://newsdata.io/api/1/news?apikey=pub_24021f4291fa33aa976aa3dddd22e4c75cb40&country=${datos.pais}&language=es&category=${datos.categoria}`)
-        const data = await respuesta.json()
-        console.log(data.results)
-        console.log(datos.pais)
-        console.log(datos.categoria)
-        setNoticias(data.results)
-        setPais(datos.pais)
-        setCategoria(datos.categoria)
-      }}
+  useEffect(() =>{
+    consultarApi()
+  }, [])
+
+  const consultarApi = async () =>{
+    try {
+      const respuesta = await fetch(API);
+      const dato = await respuesta.json();
+      console.log(dato.result);
+      setNoticias(dato.results);
+      }
     catch(errores){
       console.log(errores)
     }
-  }
+  };
 
     return (
         <section>
-            <Form onSubmit={handleSubmit(consultarApi)} className="d-flex flex-column align-items-center">
+            <Form className="d-flex flex-column align-items-center">
 
             <Form.Group className="me-4" controlId="controlPais">
             <Form.Label>Seleccionar pais:</Form.Label>
-            <Form.Select  className="formSelect" 
-            {... register("pais", 
-            {required: "Campo obligatorio"})}>
+            <Form.Select  className="formSelect" >
               <option value="">--Seleccione un pais--</option>
               <option value="ar">Argentina</option>
               <option value="de">Alemania</option>
@@ -51,14 +42,11 @@ const FormularioNoticias = () => {
               <option value="fr">Francia</option>
               <option value="us">Estados Unidos</option>
             </Form.Select>
-            <Form.Text className="text-danger">{errors.pais?.message}</Form.Text>
           </Form.Group>
 
           <Form.Group controlId="controlCategoria">
             <Form.Label className="mt-1">Seleccionar categoría:</Form.Label>
-            <Form.Select className="formSelect"  
-            {... register("categoria", 
-            {required: "Campo obligatorio"})}>
+            <Form.Select className="formSelect" >
                 <option value="">--Seleccione una categoria--</option>
                 <option value="top">Destacadas</option>
                 <option value="science">Ciencia</option>
@@ -67,8 +55,8 @@ const FormularioNoticias = () => {
                 <option value="politics">Politica</option>
                 <option value="technology">Tecnología</option>
             </Form.Select>
-            <Form.Text className="text-danger">{errors.categoria?.message}</Form.Text>
              </Form.Group>
+
              <Button variant="primary" className="my-2" type="submit">
                Enviar
             </Button>
